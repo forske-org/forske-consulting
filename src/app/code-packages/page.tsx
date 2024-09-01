6cf81fa8c136fa474447768fbc644002ee6758ca
Manuel Nielsen <MaNielsen@forske.org>
2024-09-01 13:48:03 +1000

    Updated code packages page

--- src/app/code-packages/page.tsx
+++ src/app/code-packages/page.tsx
@@ -4,8 +4,8 @@ export default async function Page () {
     return (
         <Fragment>
             <h1>Code Packages</h1>
-            <p>The Forske npm packages can be found <a href='https://www.npmjs.com/search?q=%40forske' target='_blank'>here</a></p>
-            
+            <p>The Forske node packages can be found <a href='https://www.npmjs.com/search?q=%40forske' target='_blank'>here</a></p>
+            <p>The Forske python packages can be found <a href='https://www.pypi.org' target='_blank'>here</a></p>
         </Fragment>
     )
 }
